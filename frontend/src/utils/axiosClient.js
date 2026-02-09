28b73c81748303b505421fa0c58d883f545aa3b4
Samir Kumar <samsahu545@gmail.com>
2026-02-09 18:49:28 +0530

    Fix: Logout Problem Fixed

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -9,4 +9,38 @@ const axiosClient =  axios.create({
 });
 
 
+axiosClient.interceptors.request.use(
+  (config) => {
+    config.withCredentials = true;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+axiosClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    
+    // Handle 401 errors
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      
+      // If logout endpoint, just redirect
+      if (originalRequest.url.includes('/logout')) {
+        window.location.href = '/';
+        return Promise.reject(error);
+      }
+      
+      // For other 401s, redirect to login
+      window.location.href = '/login';
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
 export default axiosClient;
